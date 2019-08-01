d1e799b5cf2f7a5eda34d4c9aa743531b1b217b6
danielfpaiva <danielfonsecapaiva@gmail.com>
2019-08-01 19:31:08 +0100

    for and while cycles

---
+++ for_while.php
@@ -0,0 +1,11 @@
+<?php
+
+for ($i = 0; $i <= 5; $i++){
+    echo $i;
+}
+
+$i=0;
+while($i <= 10){
+    echo 'Number '.$i.'<br/>';
+    $i++;
+}
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Paiva
- * Date: 01/08/2019
- * Time: 19:02
- */
