74cb44eb52eee5789625a8da34b43f8d2a6c7386
danielfpaiva <danielfonsecapaiva@gmail.com>
2019-08-01 19:49:08 +0100

    functions

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+/*function greet(){
+    echo 'Hello World';
+}
+
+greet();
+
+function greet($name){
+    echo 'Hello '. $name;
+}
+
+greet('Daniel');
+
+
+function greet($greeting, $name){
+    echo $greeting.' '. $name;
+}
+
+greet('Welcome','Daniel');
+
+function greet($greeting, $name='John'){
+    echo strtoupper($greeting).' '. $name;
+}
+
+greet('Welcome');
+
+*/
+
+$num=110;
+
+if($num != 10){
+    echo 'Correct';
+}
